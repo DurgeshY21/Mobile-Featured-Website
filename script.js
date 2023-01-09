0651ec91fea202b0af9348a307a5d0956bb3f06f
Durgesh Yadav <durgeshy5226@gmail.com>
2023-01-09 21:37:37 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var circle=document.getElementById("circle");
+var upBtn=document.getElementById("upBtn");
+var downBtn=document.getElementById("downBtn");
+
+var rotateValue=circle.style.transform;
+var rotateSum;
+
+upBtn.onClick=function()
+{
+    rotateSum=rotateValue +"rotate(-90deg)";
+    circle.style.transform=rotateSum;
+    rotateValue=rotateSum;
+}
